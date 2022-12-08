6512e3a3c2765e0c245567cd743e2fad546b7042
rajpal229 <rajpalrawat229@gmail.com>
2022-12-08 17:08:35 +0530

    Added: UC2_PerfectNumber

---
+++ PerfectNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class PerfectNumber
+    {
+        public void CheckPerfectNumber()
+        {
+            int n, i, sum;
+
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Check whether a given number is perfect number or not:\n");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Input the  number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            sum = 0;
+            Console.WriteLine("The positive divisor  : ");
+            for (i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine("{0}  ", i);
+                }
+            }
+            Console.WriteLine("\nThe sum of the divisor is : {0}", sum);
+            if (sum == n)
+                Console.WriteLine("\nSo, the number is perfect.");
+            else
+                Console.WriteLine("\nSo, the number is not perfect.");
+            Console.WriteLine("\n");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,13 +7,20 @@
             bool Flag = true;
             while (Flag)
             {
-                Console.WriteLine("1. FibonacciSeries");
+                Console.WriteLine("1. FibonacciSeries\n2. PerfectNumber");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         FibonacciSeries fibonacciSeries = new FibonacciSeries();
                         fibonacciSeries.CheckFibonacciNumber();
+                        break;
+                    case 2:
+                        PerfectNumber perfectNumber = new PerfectNumber();
+                        perfectNumber.CheckPerfectNumber();
+                        break;
+                    default:
+                        Console.WriteLine("Enter Valid Input");
                         break;
                 }
             }
