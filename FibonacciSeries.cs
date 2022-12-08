78b28c73c6b99661ea61d5bb385a3a2f55ac0968
rajpal229 <rajpalrawat229@gmail.com>
2022-12-08 17:01:03 +0530

    Added: UC1_FibonacciSeries

---
+++ FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class FibonacciSeries
+    {
+        public void CheckFibonacciNumber()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("Enter the number of elements: ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " "); //printing 0 and 1    
+            for (i = 2; i < number; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine("\n");
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LogicalProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Flag = true;
+            while (Flag)
+            {
+                Console.WriteLine("1. FibonacciSeries");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries fibonacciSeries = new FibonacciSeries();
+                        fibonacciSeries.CheckFibonacciNumber();
+                        break;
+                }
+            }
+        }
+    }
+}
