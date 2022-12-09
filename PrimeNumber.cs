504428ad46b1211515663fa008a2699f34d3c9f6
rajpal229 <rajpalrawat229@gmail.com>
2022-12-09 20:01:22 +0530

    Added: UC3_PrimeNumber

---
+++ PrimeNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class PrimeNumber
+    {
+        public void primeNumber()
+        {
+            Console.WriteLine("Enter nummber Is this Number is Prime or Composite");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                Console.WriteLine("Number is Prime");
+            else
+                Console.WriteLine("Number is not a Prime");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             bool Flag = true;
             while (Flag)
             {
-                Console.WriteLine("1. FibonacciSeries\n2. PerfectNumber");
+                Console.WriteLine("1. FibonacciSeries\n2. PerfectNumber\n3. Prime Number");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -18,6 +18,10 @@
                     case 2:
                         PerfectNumber perfectNumber = new PerfectNumber();
                         perfectNumber.CheckPerfectNumber();
+                        break;
+                    case 3:
+                        PrimeNumber primenumber = new PrimeNumber();
+                        primenumber.primeNumber();
                         break;
                     default:
                         Console.WriteLine("Enter Valid Input");
